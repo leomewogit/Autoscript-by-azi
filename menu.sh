a3669ab1f38220810859b99ddccebf1028168acb
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-25 00:39:25 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -192,7 +192,7 @@ case $opt in
 4) clear ; menu-ss ;;
 5) clear ; tendang ;;
 6) clear ; autoreboot ;;
-7) clear ; autoreboot ;;
+7) clear ; bash reboot.sh ;;
 8) clear ; restart ;;
 9) clear ; genssl ;;
 10) clear ; addhost ;;

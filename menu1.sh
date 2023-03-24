25179e43f0d77a5e17118857f55b6f9ef2e0a4a7
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-24 13:10:18 +0700

    Update menu1.sh

--- menu1.sh
+++ menu1.sh
@@ -146,7 +146,7 @@ ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 3)}' | head -1)"
 tmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $8" "substr ($9, 1 ,3)}' | head -1)"
 clear
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[39;1;92m                   ⇱ SERVER INFORMATION ⇲                      \E[0m"
+echo -e "\E[39;1;92m                   ⇱ SCRIPT PREMIUM CyberVPN ⇲                      \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "${BICyan} "                                                                      
 echo -e "${BICyan} ⇲  ${BICyan}Use Core        :  ${BIYellow}Xray-core"    

f628477a81fe0961cdb6e0315c67c0a961cecef4
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-25 22:26:17 +0700

    Update senmenu.sh

--- senmenu.sh
+++ senmenu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # // config Data
 echo -e "${GREEN}Config Data${NC}"
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu.sh"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/add-ws.sh"
 wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/add-ssws.sh"
 #wget -q -O /usr/bin/add-socks "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/add-socks.sh"
@@ -12,7 +12,7 @@ wget -q -O /usr/bin/autoreboot "https://raw.githubusercontent.com/Agunxzzz/XrayC
 wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/restart.sh"
 wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/tendang.sh"
 wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/clearlog.sh"
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/running.sh"
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/running.sh"
 wget -q -O /usr/bin/cek-trafik "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/cek-trafik.sh"
 wget -q -O /usr/bin/cek-speed "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/speedtes_cli.py"
 wget -q -O /usr/bin/cek-bandwidth "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/cek-bandwidth.sh"
@@ -24,12 +24,12 @@ wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Agunxzzz/XrayC
 wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-ss.sh"
 wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-trojan.sh"
 #wget -q -O /usr/bin/menu-trgo "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-trgo.sh"
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-ssh.sh"
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu-ssh.sh"
 #wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-bckp-telegram.sh"
 wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-bckp-github.sh"
 #wget -q -O /usr/bin/bckp "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/bckpbot.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/usernew.sh"
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu.sh"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 wget -q -O /usr/bin/wbm "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/webmin.sh"
 wget -q -O /usr/bin/changer "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/changer.sh"
 wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/xp.sh"
@@ -37,9 +37,9 @@ wget -q -O /usr/bin/addhost  "https://raw.githubusercontent.com/Agunxzzz/XrayCol
 wget -q -O /usr/bin/genssl  "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/genssl.sh"
 wget -q -O /usr/bin/fix "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/cf.sh"
 #wget -q -O /usr/bin/update "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/update.sh"
-wget -q -O /etc/issue.net "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/issue.net"
-wget -q -O /root/versi "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/versibasic"
-wget -q -O /usr/bin/updatsc "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/update.sh"
+wget -q -O /etc/issue.net "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/issue.net"
+wget -q -O /root/versi "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/versibasic"
+wget -q -O /usr/bin/updatsc "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/update.sh"
 
 chmod +x /usr/bin/menu
 chmod +x /usr/bin/add-ws

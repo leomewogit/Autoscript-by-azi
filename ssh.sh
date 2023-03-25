687d3ffacce65e031010b693891277f7b476c20c
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-25 20:18:44 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -112,7 +112,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/nginx.conf.txt"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://gitlab.com/hidessh/baru/-/raw/main/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/vps.conf.txt"
 /etc/init.d/nginx restart
 
 # install badvpn

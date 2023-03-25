a4a114e9c0aeeec97ddaa0bc3abc3782ed58f9ae
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-25 20:36:06 +0700

    Update vlu4a.sh

--- vlu4a.sh
+++ vlu4a.sh
@@ -76,6 +76,30 @@ apt dist-upgrade -y
 clear
 clear && clear && clear
 clear;clear;clear
+
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "memeriksa vps anda"
+sleep 1
+CEKEXPIRED () {
+        today=$(date -d +1day +%Y -%m -%d)
+        Exp1=$(curl -sS https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/izin | grep $MYIP | awk '{print $3}')
+        if [[ $today < $Exp1 ]]; then
+        echo "status script aktif.."
+        else
+        echo "SCRIPT ANDA EXPIRED";
+        exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/izin | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo "IZIN DI TERIMA!!"
+CEKEXPIRED
+else
+echo "IZIN DI TOLAK!!";
+exit 0
+fi
+
 read -p "Input Your Domain : " domain
 if [[ $domain == "" ]]; then
 clear

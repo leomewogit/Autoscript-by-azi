2e358b8bef191f7b0fb12c786dad0d659a063828
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-27 05:58:03 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -6,7 +6,8 @@ rm -rf /root/versi
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 wget -q -O /root/versi "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/versi"
 wget https://raw.githubusercontent.com/Azigaming404/CyberCHAT_BOT/main/requirements.txt
-
+https://raw.githubusercontent.com/Azigaming404/CyberCHAT_BOT/main/main.py
+pip3 install -r requirements.txt
 echo -e "\e[32mUpdate Selesai...\e[0m"
 chmod 777 /usr/bin/menu
 chmod 777 /root/versi

9177785bcf28ce8cf53dffa45cc920baa98c61db
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-27 06:18:40 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -9,8 +9,10 @@ sleep 1
 echo "\e[32mInstalling bot...\e[0m"
 wget https://raw.githubusercontent.com/Azigaming404/CyberCHAT_BOT/main/requirements.txt
 wget https://raw.githubusercontent.com/Azigaming404/CyberCHAT_BOT/main/main.py
+sudo apt install python3-pip
 pip3 install -r requirements.txt
 echo -e "\e[32mUpdate Selesai...\e[0m"
 chmod 777 /usr/bin/menu
 chmod 777 /root/versi
+rm -rf updateyes.sh
 menu

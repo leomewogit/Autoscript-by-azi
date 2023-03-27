47b6a35b792ed5289e3367b293e917ffd92afe36
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-27 07:04:25 +0700

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -5,6 +5,6 @@ if [ $LocalVersion = $Version ]; then
 echo -e "\e[32mScript Sudah Versi Terbaru...\e[0m"
 else
 echo -e "\e[32mAda Update Script!!\e[0m";
-wget -q -O updateyes.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/update/updateyes.sh && chmod +x updateyes.sh && ./updateyes.sh
+wget -q -O updateyes.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/updateyes.sh && chmod +x updateyes.sh && ./updateyes.sh
 rm -f updateyes.sh
 fi

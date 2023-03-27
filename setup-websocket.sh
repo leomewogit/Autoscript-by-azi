62ff983931601a126aa92ca0506711a17d6b834e
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-28 00:19:08 +0700

    Update setup-websocket.sh

--- setup-websocket.sh
+++ setup-websocket.sh
@@ -1,5 +1,5 @@
 clear
-https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/setup.sh && chmod +x setup.sh && ./setup.sh
+wget https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/setup.sh && chmod +x setup.sh && ./setup.sh
 rm -rf /usr/bin/menu
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 chmod 777 /usr/bin/menu

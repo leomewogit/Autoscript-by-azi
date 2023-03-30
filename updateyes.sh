c2ad2ad121b22a1d339a75d9afeadc7be20e0ab4
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-31 03:23:49 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 echo -e cheking update
 sleep 2
-wget https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/setup-websocket.sh
-bash setup-websocket.sh
 rm -f /usr/bin/menu
 rm -rf /root/versi
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/menu.sh"
 wget -q -O /root/versi "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/versi"
 sleep 1
 echo -e "\e[32mUpdate Selesai...\e[0m"

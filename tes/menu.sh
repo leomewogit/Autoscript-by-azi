126b3a1beeaf2e77ab5db569db9932940a3cda27
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-31 21:56:44 +0700

    Update menu.sh

--- tes/menu.sh
+++ tes/menu.sh
@@ -186,6 +186,7 @@ echo -e "\E[44;1;39m            ⇱ STATUS LAYANAN ⇲                 \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e ""
 echo -e " [ ${GREEN}SSH WebSocket${NC} : ${GREEN}ON ${NC}] [ ${GREEN}XRAY${NC} : ${status_xray} ]"  
+echo -e
 echo -e "                  [ ${GREEN}NGINX${NC} : ${status_nginx} ]"
 echo -e "${ungu}┌───────────────────────────────────────────────┐${NC}"
 echo -e "${ungu}│\033[0m ${BOLD}${YELLOW}SSH  VMESS   VLESS  TROJAN   SHADOWSOCKS$NC"

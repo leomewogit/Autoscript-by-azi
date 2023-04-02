2a2eba32a1fc1d6982de331f4f4e3b1e959791db
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-02 19:18:59 +0700

    Update usernew.sh

--- menu/usernew.sh
+++ menu/usernew.sh
@@ -11,6 +11,17 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+export ungu='\033[0;35m'
+
+
 # Getting
 
 domain=$(cat /etc/xray/domain)

c630e14b3c4bc36a982d0a55b04db59903cd0f6f
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-04 00:02:51 +0700

    Update myvpn.sh

--- myvpn.sh
+++ myvpn.sh
@@ -142,7 +142,7 @@ echo -e "${OKEY} Your Domain : $domain"
 sleep 2
 wget -q -O janggut.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/janggut.sh && chmod +x janggut.sh && ./janggut.sh
 sleep 1
-wget -q -O ins-xray.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget -q -O ins-xray.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 sleep 1
 wget -q -O senmenu.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/senmenu.sh && chmod +x senmenu.sh && ./senmenu.sh
 sleep 1

694059bb18d716e6be35921b58392dab8e04ebaf
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-04 22:29:00 +0700

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -136,8 +136,8 @@ wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Agunxzz
 
 wget -q -O xraymode.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/Xray%20mode/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
 sleep 1 
-wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/xray.conf && chmod +x xray.conf && ./xray.conf"
-sleep 1 
+wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/xray.conf"
+sleep 1
 
 # Installing Xray Service
 rm -fr /etc/systemd/system/xray.service.d

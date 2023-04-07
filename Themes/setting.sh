0d90ce939f30f0f9767f27e10d8b3296cf583af0
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-07 07:28:00 +0700

    Update setting.sh

--- Themes/setting.sh
+++ Themes/setting.sh
@@ -44,59 +44,72 @@ addhost
 fi
 if [ $bro = 2 ] || [ $bro = 2 ]
 then
+clear
 genssl
 fi
 if [ $bro = 3 ] || [ $bro = 3 ]
 then
+clear
 fix
 fi
 if [ $bro = 4 ] || [ $bro = 4 ]
 then
+clear
 tema
 fi
 if [ $bro = 5 ] || [ $bro = 5 ]
 then
+clear
 cek-speed
 fi
 if [ $bro = 6 ] || [ $bro = 6 ]
 then
+clear
 echo "kill all proces in your cpu"
 killall /bin/bash /usr/bin/menu
 fi
 if [ $bro = 7 ] || [ $bro = 7 ]
 then
+clear
 clearlog
 fi
 if [ $bro = 8 ] || [ $bro = 8 ]
 then
+clear
 echo "Bobo dulu ye guys" | lolcat
 sleep 2
 reboot
 fi
 if [ $bro = 9 ] || [ $bro = 9 ]
 then
+clear
 autoreboot
 fi
 if [ $bro = 10 ] || [ $bro = 10 ]
 then
+clear
 nano /etc/issue.net
 fi
 
 if [ $bro = 11 ] || [ $bro = 11 ]
 then
+clear
 tendang
 fi
 if [ $bro = 12 ] || [ $bro = 12 ]
 then
+clear
 limitspeed
 fi
 if [ $bro = 13 ] || [ $bro = 13 ]
 then
+clear
 restart
 fi
 
 if [ $bro = 14 ] || [ $bro = 14 ]
 then
+clear
 neofetch
 echo "${BlueCyan}==================================================="
 echo "{WhiteB}              Credit Script By : Azigaming404"

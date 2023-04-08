34ccb91294a781c32c704ad4c5ad78ada0c8b1a6
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-08 14:12:38 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -4,7 +4,7 @@ sleep 2
 rm -f /usr/bin/setting
 rm -f /usr/bin/menu
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/menu.sh"
-https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Themes/setting.sh
+wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Themes/setting.sh"
 sleep 1
 echo -e "\e[32mUpdate Selesai...\e[0m"
 chmod 777 /usr/bin/menu

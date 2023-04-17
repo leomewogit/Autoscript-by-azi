04d24b140a3f1c5f4f58d1029c73c6a5f8da23b4
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-17 23:54:15 +0700

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -130,7 +130,7 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/nginx.conf.txt" 
+wget -q -O /etc/nginx/nginx.conf "https://gitlab.com/hidessh/baru/-/raw/main/nginx.conf" 
 mkdir -p /home/vps/public_html
 wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/vps.conf.txt"
 

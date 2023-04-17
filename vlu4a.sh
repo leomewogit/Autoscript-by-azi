79407192844e5a0754400f36a891206c1ed1f70f
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-17 17:41:44 +0700

    Update vlu4a.sh

--- vlu4a.sh
+++ vlu4a.sh
@@ -118,7 +118,7 @@ echo -e "${OKEY} Your Domain : $domain"
 sleep 2
 wget -q -O janggut.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/janggut.sh && chmod +x janggut.sh && ./janggut.sh
 sleep 1
-wget -q -O ins-xray.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget -q -O ins-xray.sh https://raw.githubusercontent.com/Agunxzzz/Mina-Xray-SSH/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 sleep 1
 wget -q -O senmenu.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/senmenu.sh && chmod +x senmenu.sh && ./senmenu.sh
 sleep 1

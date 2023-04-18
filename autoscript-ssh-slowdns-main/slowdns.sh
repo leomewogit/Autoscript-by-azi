f3ea05a968769a0bf361be99d52e6efdcb93d41e
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-19 05:51:26 +0700

    Update slowdns.sh

--- autoscript-ssh-slowdns-main/slowdns.sh
+++ autoscript-ssh-slowdns-main/slowdns.sh
@@ -44,7 +44,7 @@ service cron restart
 
 #tambahan port openssh
 cd
-echo "Port 2222" >> /etc/ssh/sshd_config
+echo "Port 22" >> /etc/ssh/sshd_config
 echo "Port 2269" >> /etc/ssh/sshd_config
 sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
 service ssh restart

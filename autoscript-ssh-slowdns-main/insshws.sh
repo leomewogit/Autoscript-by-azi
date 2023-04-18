d2b43399d2eb1cac4100e61b8501588fbb55a6d1
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-18 16:09:32 +0700

    Update insshws.sh

--- autoscript-ssh-slowdns-main/insshws.sh
+++ autoscript-ssh-slowdns-main/insshws.sh
@@ -5,7 +5,7 @@ cd
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/ws-stunnel
 wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/sreyaeve/sshxrayv3/main/FILE/OPENVPN/ws-ovpn.py
-wget -O /usr/local/bin/openssh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/openssh-socket.py
+wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/openssh-socket.py
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 chmod +x /usr/local/bin/ws-ovpn
@@ -14,7 +14,7 @@ chmod +x /usr/local/bin/openssh
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/sreyaeve/sshxrayv3/main/FILE/WEBSOCKET/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/sreyaeve/sshxrayv3/main/FILE/WEBSOCKET/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/sreyaeve/sshxrayv3/main/FILE/OPENVPN/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
-
+wget -O /etc/systemd/system/edu-proxy.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/http.service && chmod +x /etc/systemd/system/edu-proxy.service
 systemctl daemon-reload
 systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service

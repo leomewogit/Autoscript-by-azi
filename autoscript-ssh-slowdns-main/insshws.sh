e517145bd300318458d30ca7f8c84f3b22c7abad
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-18 15:53:28 +0700

    Update insshws.sh

--- autoscript-ssh-slowdns-main/insshws.sh
+++ autoscript-ssh-slowdns-main/insshws.sh
@@ -3,7 +3,7 @@
 cd
 
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/sreyaeve/sshxrayv3/main/FILE/WEBSOCKET/ws-stunnel
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/ws-stunnel
 wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/sreyaeve/sshxrayv3/main/FILE/OPENVPN/ws-ovpn.py
 
 chmod +x /usr/local/bin/ws-dropbear

84510ceccc418280a5edea7a7ed58ec3543e4df9
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-18 19:10:32 +0700

    Update ssh.sh

--- autoscript-ssh-slowdns-main/ssh.sh
+++ autoscript-ssh-slowdns-main/ssh.sh
@@ -355,7 +355,8 @@ wget https://raw.githubusercontent.com/Azigaming404/websocket/main/websocket.sh 
 clear
 #installer OPH
 wget https://gitlab.com/hidessh/baru/-/raw/main/ohp.sh && chmod +x ohp.sh && ./ohp.sh
-
+#installer openvpn
+https://raw.githubusercontent.com/sreyaeve/sshxrayv3/main/FILE/OPENVPN/vpn.sh
 #remove file 
 cd
 rm -rf hideinstall-websocket.sh

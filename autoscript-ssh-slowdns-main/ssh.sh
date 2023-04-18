1e5e73d80dec3b3f1c9d6ce56f2d0e71e47101cc
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-19 04:45:20 +0700

    Update ssh.sh

--- autoscript-ssh-slowdns-main/ssh.sh
+++ autoscript-ssh-slowdns-main/ssh.sh
@@ -118,7 +118,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/nginx.conf.txt"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://gitlab.com/hidessh/baru/-/raw/main/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/vps.conf.txt"
 /etc/init.d/nginx restart
 
 

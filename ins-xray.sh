0ea0a5a722926ba6caa55b0564cce93b919ef8a9
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-19 14:37:21 +0700

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -130,14 +130,14 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/nginx.conf.txt" 
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/potatonc/ScriptAutoInstallPotato/master/nginx/default" 
 mkdir -p /home/vps/public_html
 wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/vps.conf.txt"
 
 
 wget -q -O xraymode.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/Xray%20mode/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
 sleep 1 
-wget -q -O xray.conf https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/xray.conf.txt && chmod +x xray.conf && ./xray.conf
+wget -q -O xray.conf https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/xray.conf.txt && chmod +x xray.conf && ./xray.conf
 sleep 1
 
 # Installing Xray Service

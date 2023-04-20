7a432d90e20ed116849b45695ffbacc6aa240241
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-21 01:55:32 +0700

    Update genssl.sh

--- genssl.sh
+++ genssl.sh
@@ -18,7 +18,7 @@ sleep 2
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/hidessh/stunnel.pem --keypath /etc/hidessh/stunnel.pem --ecc
 echo -e "[ ${GREEN}INFO${NC} ] Renew gen-ssl done... " 
 sleep 2
 echo -e "[ ${GREEN}INFO${NC} ] Starting service $Cek " 

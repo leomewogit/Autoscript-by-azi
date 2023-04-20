5e335e47529b5fce1fbdeacfcab7ed97f75372d2
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-21 01:45:50 +0700

    Update myvpn.sh

--- myvpn.sh
+++ myvpn.sh
@@ -138,7 +138,7 @@ chmod +x /root/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath  /etc/hidessh/stunnel.pem --keypath /etc/hidessh/stunnel.pem --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath  /etc/stunnel/stunnel.pem --keypath /etc/stunnel/stunnel.pem --ecc
 echo -e "${OKEY} Your Domain : $domain"
 sleep 2
 echo "install ssh lurd..!!" | lolcat

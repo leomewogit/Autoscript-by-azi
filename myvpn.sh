d38d17349479a1188746c509f933192629716095
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-21 01:43:05 +0700

    Update myvpn.sh

--- myvpn.sh
+++ myvpn.sh
@@ -130,6 +130,7 @@ echo -e "[ ${GREEN}INFO${NC} ] Starting renew cert... "
 sleep 2
 echo -e "${OKEY} Starting Generating Certificate"
 rm -fr /root/.acme.sh
+mkdir /etc/hidessh
 mkdir -p /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
 chmod +x /root/.acme.sh/acme.sh
@@ -137,7 +138,7 @@ chmod +x /root/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath  /etc/hidessh/stunnel.pem --keypath /etc/hidessh/stunnel.pem --ecc
 echo -e "${OKEY} Your Domain : $domain"
 sleep 2
 echo "install ssh lurd..!!" | lolcat

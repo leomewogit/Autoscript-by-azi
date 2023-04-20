4b49e8103462b5cd5ba433d3af7ff7b8720b93b7
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-21 02:21:31 +0700

    Update myvpn.sh

--- myvpn.sh
+++ myvpn.sh
@@ -138,7 +138,7 @@ chmod +x /root/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath  /etc/hidessh/stunnel.pem --keypath /etc/hidessh/stunnel.pem --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 echo -e "${OKEY} Your Domain : $domain"
 sleep 2
 echo "install xray lurd...!!!" | lolcat

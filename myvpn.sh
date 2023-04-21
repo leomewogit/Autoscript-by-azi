9efde936423c128916d16faf9468589e87c8e8ab
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-21 13:11:19 +0700

    Update myvpn.sh

--- myvpn.sh
+++ myvpn.sh
@@ -97,7 +97,7 @@ echo -e "$green      Install Websocket              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/andristji/Xray-SSH/main/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Insshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 
 #exp
 cd /usr/bin

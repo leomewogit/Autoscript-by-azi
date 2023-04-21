7271312064964c8d9d0df466242b31e4a963b4b6
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-21 23:54:55 +0700

    Update myvpn.sh

--- myvpn.sh
+++ myvpn.sh
@@ -116,7 +116,7 @@ rm -f /root/insshws.sh
 rm -f /root/xraymode.sh
 
 #xray
-wget -q -O ins-xray.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget -q -O ins-xray.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 sleep 1
 wget -q -O senmenu.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/senmenu.sh && chmod +x senmenu.sh && ./senmenu.sh
 #cronjob

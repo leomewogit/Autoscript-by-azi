0be1b9bc0ae632fb0d917063dc929451abd9feee
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-21 12:28:52 +0700

    Update kanyut.sh

--- kanyut.sh
+++ kanyut.sh
@@ -99,6 +99,6 @@ rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
 rm -rf /root/vnstat-2.6 >/dev/null 2>&1
 
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-yellow "Deby cantik..."
+yellow "Done Bangsat...!!"
 sleep 3
 clear

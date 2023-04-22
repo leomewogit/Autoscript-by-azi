61f37b47914cf786e34a5e800098910db8437614
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-22 16:03:16 +0700

    Update ipsec.sh

--- ipsec/ipsec.sh
+++ ipsec/ipsec.sh
@@ -306,7 +306,7 @@ wget -O /usr/bin/addl2tp https://raw.githubusercontent.com/Azigaming404/Autoscri
 wget -O /usr/bin/dell2tp https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/ipsec/dell2tp.sh && chmod +x /usr/bin/dell2tp
 wget -O /usr/bin/addpptp https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/ipsec/addpptp.sh && chmod +x /usr/bin/addpptp
 wget -O /usr/bin/delpptp https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/ipsec/delpptp.sh && chmod +x /usr/bin/delpptp
-wget -O /usr/bin/renewpptp https://${akbarvpn}/renewpptp.sh && chmod +x /usr/bin/renewpptp
+wget -O /usr/bin/renewpptp https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/ipsec/renewpptp.sh && chmod +x /usr/bin/renewpptp
 wget -O /usr/bin/renewl2tp https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/ipsec/renewl2tp.sh && chmod +x /usr/bin/renewl2tp
 touch /var/lib/crot/data-user-l2tp
 touch /var/lib/crot/data-user-pptp

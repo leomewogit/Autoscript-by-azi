77168f6257bab42efffdc94585e1df32f4374907
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-23 13:48:46 +0700

    Update addl2tp.sh

--- ipsec/addl2tp.sh
+++ ipsec/addl2tp.sh
@@ -11,6 +11,21 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+blue='\033[0;34m'
+ungu='\033[0;35m'
+Green="\033[32m"
+Red="\033[31m"
+WhiteB="\e[5;37m"
+BlueCyan="\e[5;36m"
+Green_background="\033[42;37m"
+Red_background="\033[41;37m"
+Suffix="\033[0m"
+
+
 BIBlack='\033[1;90m'      # Black
 BIRed='\033[1;91m'        # Red
 BIGreen='\033[1;92m'      # Green

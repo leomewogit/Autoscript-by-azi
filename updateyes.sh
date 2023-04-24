335e9b37a4f31fdc57e72135b0c65e792cb97c8f
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-25 00:06:42 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -3,11 +3,12 @@ echo -e cheking update
 sleep 2
 rm -f /usr/bin/setting
 rm -f /usr/bin/menu
+rm -f /usr/bin/usernew
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/menu.sh"
 wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Themes/setting.sh"
 wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/Azigaming404/websocket/main/https.py && chmod +x /usr/local/bin/edu-proxy
 wget -O /etc/systemd/system/edu-proxy.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/http.service && chmod +x /etc/systemd/system/edu-proxy.service
-
+wget -q-O /usr/bin/usernew "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu/usernew.sh"
 systemctl daemon-reload
 systemctl enable edu-proxy.service
 systemctl start edu-proxy.service
@@ -15,5 +16,6 @@ systemctl restart edu-proxy.service
 echo -e "\e[32mUpdate Selesai...\e[0m"
 chmod 777 /usr/bin/menu
 chmod 777 /usr/bin/setting
+chmod 777 /usr/bin/usernew
 rm -rf updateyes.sh
 menu

e63e09f5251ce12039211fdd94be965bc91ab610
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-27 20:10:38 +0700

    Update delet-vmess.sh

--- tes/delet-vmess.sh
+++ tes/delet-vmess.sh
@@ -36,7 +36,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot/data-user-l2tp")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### " "/var/lib/crot/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select One Client[1]: " CLIENT_NUMBER
@@ -45,15 +45,15 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot/data-user-l2tp")
 		fi
 	done
 # match the selected number to a client name
-VPN_USER=$(grep -E "^### " "/var/lib/crot/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/var/lib/crot/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+VPN_USER=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 # Delete VPN user
 sed -i '/^"'"$VPN_USER"'" l2tpd/d' /etc/ppp/chap-secrets
 # shellcheck disable=SC2016
 sed -i '/^'"$VPN_USER"':\$1\$/d' /etc/ipsec.d/passwd
 sed -i "/^### $VPN_USER $exp/d" /var/lib/crot/data-user-l2tp
 # Update file attributes
-chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
+
 clear
 echo ""
 echo "=========================="
@@ -62,4 +62,4 @@ echo "=========================="
 echo "Username  : $VPN_USER"
 echo "Expired   : $exp"
 echo "=========================="
-echo "Script Mod By SL"
+echo "Beta Delet"

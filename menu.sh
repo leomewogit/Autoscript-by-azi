69f6486fc4a0c4ee3a5d4d7dddac0273e38a95d6
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-28 00:33:58 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -101,7 +101,7 @@ else
 fi
 clear
 echo -e "\033[1;93m──────────────────────────────────────────\033[0m"
-echo -e "\033[42m          ∆ INFO VPS ∆   \033[0m"
+echo -e "\033[42m                ∆ INFO VPS ∆                  \033[0m"
 echo -e "\033[1;93m──────────────────────────────────────────\033[0m"
 echo -e " ${YELL}System OS${NC}     : \033[0;32m$MODEL${NC}"
 echo -e " ${YELL}Server RAM${NC}    : \033[0;32m$RAM MB $NC"

993aa9c650138253962bdc16a08e04c446783de3
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-28 00:53:24 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -49,6 +49,33 @@ export Server_IP="underfined"
 export Script_Mode="Stable"
 export Auther="XdrgVPN"
 
+# status
+rm -rf /root/status
+wget -q -O /root/status "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/statushariini"
+
+
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "memeriksa vps anda"
+sleep 0.5
+CEKEXPIRED () {
+        today=$(date -d +1day +%Y -%m -%d)
+        Exp1=$(curl -sS https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/izin | grep $MYIP | awk '{print $3}')
+        if [[ $today < $Exp1 ]]; then
+        echo "status script aktif.."
+        else
+        echo "SCRIPT ANDA EXPIRED";
+        exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/izin | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo "IZIN DI TERIMA!!"
+CEKEXPIRED
+else
+echo "Akses di tolak!! Benget sia hurung!!";
+exit 0
+fi
 # // Root Checking
 if [ "${EUID}" -ne 0 ]; then
                 echo -e "${EROR} Please Run This Script As Root User !"

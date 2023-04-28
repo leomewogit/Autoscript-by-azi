41706578060703083221360be70446b9ee8aeb10
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-28 15:02:23 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -4,14 +4,30 @@ sleep 2
 rm -f /usr/bin/setting
 rm -f /usr/bin/menu
 rm -f /usr/bin/usernew
+rm -f /usr/bin/menu-ss
+rm -f /usr/bin/menu-vmess
+rm -f /usr/bin/menu-vless
+rm -f /usr/bin/menu-trojan
+
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Themes/setting.sh"
 
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/menu-ss.sh"
+
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/menu-vmess.sh"
+
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/menu-vless.sh"
+
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/tes/menu-trojan.sh"
 
 
 
 chmod 777 /usr/bin/menu
 chmod 777 /usr/bin/setting
+chmod 777 /usr/bin/menu-ss 
+chmod 777 usr/bin/menu-vmess
+chmod 777 /usr/bin/menu-vless
+chmod 777 /usr/bin/menu-trojan
 
 rm -rf updateyes.sh
 menu

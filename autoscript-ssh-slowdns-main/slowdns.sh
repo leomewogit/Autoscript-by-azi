a8090506528ec282ed1d4696d877828f6f378607
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-29 17:05:50 +0700

    Update slowdns.sh

--- autoscript-ssh-slowdns-main/slowdns.sh
+++ autoscript-ssh-slowdns-main/slowdns.sh
@@ -44,13 +44,8 @@ apt install -y dos2unix debconf-utils
 service cron reload
 service cron restart
 
-#tambahan port openssh
-cd
-echo "Port 2222" >> /etc/ssh/sshd_config
-echo "Port 2269" >> /etc/ssh/sshd_config
-sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
-service ssh restart
-service sshd restart
+
+
 
 #konfigurasi slowdns
 rm -rf /etc/slowdns
